9a3820ea1f896ab9e483ea7573c71cfb7d546d38
Kritika Pudasaini <kritikapudasaini23@gmail.com>
2025-01-08 13:12:21 +0545

    Your commit message here

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,3 @@
-
-// import 'package:class_test_1/myform.dart';
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
- 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'class test',
-//       theme: ThemeData(
-      
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: MYForm(),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class Student {
